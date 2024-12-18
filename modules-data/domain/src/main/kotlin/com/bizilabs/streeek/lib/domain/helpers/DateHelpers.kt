64dd0553c6ee4ee1ed729327c8d1d91eb97b1f63
theophiluskibet <kibettheophilus@gmail.com>
2024-12-18 12:49:07 +0300

    add formats to dates

--- modules-data/domain/src/main/kotlin/com/bizilabs/streeek/lib/domain/helpers/DateHelpers.kt
+++ modules-data/domain/src/main/kotlin/com/bizilabs/streeek/lib/domain/helpers/DateHelpers.kt
@@ -3,6 +3,9 @@ package com.bizilabs.streeek.lib.domain.helpers
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.format.DateTimeComponents
+import kotlinx.datetime.format.FormatStringsInDatetimeFormats
+import kotlinx.datetime.format.byUnicodePattern
 import kotlinx.datetime.minus
 import kotlinx.datetime.toLocalDateTime
 
@@ -11,9 +14,11 @@ object DateFormats {
     const val EEE_MMM_dd_yyyy_HH_mm = "EEE, MMM dd yyyy HH:mm a"
 }
 
+@OptIn(FormatStringsInDatetimeFormats::class)
 fun String.asDate(format: String = DateFormats.ISO_8601): Instant? {
     return tryOrNull {
-        Instant.parse(this)
+        val formatter = DateTimeComponents.Format { byUnicodePattern(format) }
+        Instant.parse(this, formatter)
     }
 }
 
@@ -21,15 +26,19 @@ fun Instant.asString(format: String = DateFormats.EEE_MMM_dd_yyyy_HH_mm): String
     return this.toLocalDateTime(kotlinx.datetime.TimeZone.UTC).asString(format = format)
 }
 
+@OptIn(FormatStringsInDatetimeFormats::class)
 fun LocalDate.asString(format: String = DateFormats.EEE_MMM_dd_yyyy_HH_mm): String? {
     return tryOrNull {
-        this.toString()
+        val formatter = LocalDate.Format { byUnicodePattern(format) }
+        formatter.format(this)
     }
 }
 
+@OptIn(FormatStringsInDatetimeFormats::class)
 fun LocalDateTime.asString(format: String = DateFormats.EEE_MMM_dd_yyyy_HH_mm): String? {
     return tryOrNull {
-        this.toString()
+        val formatter = LocalDateTime.Format { byUnicodePattern(format) }
+        formatter.format(this)
     }
 }
 
