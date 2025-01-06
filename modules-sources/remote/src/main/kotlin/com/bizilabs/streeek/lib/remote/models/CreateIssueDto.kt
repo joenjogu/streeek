8d4c8fbf6fabac55c8927956680b421c675861ad
theophiluskibet <kibettheophilus@gmail.com>
2025-01-06 14:33:42 +0300

    add comment for issue labels

--- modules-sources/remote/src/main/kotlin/com/bizilabs/streeek/lib/remote/models/CreateIssueDto.kt
+++ modules-sources/remote/src/main/kotlin/com/bizilabs/streeek/lib/remote/models/CreateIssueDto.kt
@@ -3,5 +3,5 @@ package com.bizilabs.streeek.lib.remote.models
 data class CreateIssueDto(
     val title: String,
     val body: String,
-    val label: String
+    val label: String // drop down for feature, bug , enhancement
 )
