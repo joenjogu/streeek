db54ce7763debc5e363ff399b7731f7ff05feaa5
theophiluskibet <kibettheophilus@gmail.com>
2025-01-06 11:21:39 +0300

    add get issues remote source impl

--- modules-sources/remote/src/main/kotlin/com/bizilabs/streeek/lib/remote/sources/issues/IssuesRemoteSource.kt
+++ modules-sources/remote/src/main/kotlin/com/bizilabs/streeek/lib/remote/sources/issues/IssuesRemoteSource.kt
@@ -4,13 +4,18 @@ import com.bizilabs.streeek.lib.remote.helpers.GithubEndpoint
 import com.bizilabs.streeek.lib.remote.helpers.NetworkResult
 import com.bizilabs.streeek.lib.remote.helpers.safeApiCall
 import com.bizilabs.streeek.lib.remote.models.CreateIssueDto
+import com.bizilabs.streeek.lib.remote.models.GithubIssueDTO
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
 
 interface IssuesRemoteSource {
     suspend fun createIssue(createIssueDto: CreateIssueDto): NetworkResult<Boolean>
+    suspend fun getIssues(name: String): NetworkResult<List<GithubIssueDTO>>
 }
 
 class IssuesRemoteSourceImpl(
@@ -21,6 +26,14 @@ class IssuesRemoteSourceImpl(
             client.post {
                 url(GithubEndpoint.Issues.url)
                 setBody(body = createIssueDto)
+            }
+        }
+
+    override suspend fun getIssues(name: String): NetworkResult<List<GithubIssueDTO>> =
+        safeApiCall {
+            client.get {
+                url(GithubEndpoint.Issues.url)
+                parameter("creator", name)
             }
         }
 }
