e59b6764e3743c92c89fa891af419fea2ca7e2d5
theophiluskibet <kibettheophilus@gmail.com>
2025-01-28 23:16:14 +0300

    add text and setup preview for ReminderScreenContent

--- modules-features/reminders/src/main/java/com/bizilabs/streeek/feature/reminders/single/ReminderScreen.kt
+++ modules-features/reminders/src/main/java/com/bizilabs/streeek/feature/reminders/single/ReminderScreen.kt
@@ -1,10 +1,13 @@
 package com.bizilabs.streeek.feature.reminders.single
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 
@@ -12,14 +15,22 @@ class ReminderScreen(val label: String, val day: Int, val code: Int) : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
-        ReminderScreenContent()
+        ReminderScreenContent(label)
     }
 }
 
 @Composable
-fun ReminderScreenContent() {
+fun ReminderScreenContent(label: String) {
     Scaffold { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
+            Text("Welcome to this screen: $label")
         }
     }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ReminderScreenContentPreview(modifier: Modifier = Modifier) {
+    ReminderScreenContent("")
 }
