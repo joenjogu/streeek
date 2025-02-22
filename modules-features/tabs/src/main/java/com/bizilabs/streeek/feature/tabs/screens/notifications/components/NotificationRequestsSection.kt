9d54213ecdaca28b208d38d7bc06e610d0929303
mambo <mambobryan@gmail.com>
2025-02-22 12:45:00 +0300

    closes #144 ; fixes empty state for request

--- modules-features/tabs/src/main/java/com/bizilabs/streeek/feature/tabs/screens/notifications/components/NotificationRequestsSection.kt
+++ modules-features/tabs/src/main/java/com/bizilabs/streeek/feature/tabs/screens/notifications/components/NotificationRequestsSection.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CheckCircle
 import androidx.compose.material.icons.rounded.Error
+import androidx.compose.material.icons.rounded.People
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -33,7 +34,9 @@ import coil.compose.AsyncImage
 import com.bizilabs.streeek.feature.tabs.screens.notifications.NotificationsScreenState
 import com.bizilabs.streeek.lib.common.components.paging.SafiPagingComponent
 import com.bizilabs.streeek.lib.common.models.FetchState
+import com.bizilabs.streeek.lib.design.components.SafiCenteredColumn
 import com.bizilabs.streeek.lib.design.components.SafiCircularProgressIndicator
+import com.bizilabs.streeek.lib.design.components.SafiInfoSection
 import com.bizilabs.streeek.lib.design.components.SafiRefreshBox
 import com.bizilabs.streeek.lib.domain.extensions.asCount
 import com.bizilabs.streeek.lib.domain.models.team.MemberAccountRequestDomain
@@ -54,6 +57,20 @@ fun NotificationRequestsSection(
         SafiPagingComponent(
             modifier = Modifier.fillMaxSize(),
             data = data,
+            refreshEmpty = {
+                SafiCenteredColumn(
+                    modifier =
+                        Modifier
+                            .fillMaxSize(),
+                ) {
+                    SafiInfoSection(
+                        icon = Icons.Rounded.People,
+                        title = "No Requests Currently",
+                        description = "",
+                    ) {
+                    }
+                }
+            },
         ) { value ->
             JoinRequestComponent(value, state, onClickCancelRequest)
         }
