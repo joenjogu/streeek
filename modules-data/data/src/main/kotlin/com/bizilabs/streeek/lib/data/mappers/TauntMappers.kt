980996de538a134ca67ebdb9540df1d1cc95e6d9
theophiluskibet <kibettheophilus@gmail.com>
2025-02-24 21:09:48 +0300

    add taunt mapper

---
+++ modules-data/data/src/main/kotlin/com/bizilabs/streeek/lib/data/mappers/TauntMappers.kt
@@ -0,0 +1,9 @@
+package com.bizilabs.streeek.lib.data.mappers
+
+import com.bizilabs.streeek.lib.domain.models.TauntDomain
+import com.bizilabs.streeek.lib.remote.models.TauntDTO
+
+fun TauntDTO.toDomain() = TauntDomain(
+    success = success,
+    message = message
+)
