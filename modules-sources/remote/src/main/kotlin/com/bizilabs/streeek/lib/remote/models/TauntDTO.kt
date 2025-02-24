eaccc0c0cdecab8da7cda4f8bdf7d99b6ad9351a
theophiluskibet <kibettheophilus@gmail.com>
2025-02-24 21:09:37 +0300

    remove body in  TauntDTO

--- modules-sources/remote/src/main/kotlin/com/bizilabs/streeek/lib/remote/models/TauntDTO.kt
+++ modules-sources/remote/src/main/kotlin/com/bizilabs/streeek/lib/remote/models/TauntDTO.kt
@@ -5,6 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class TauntDTO(
     val success: Boolean,
-    val message: String,
-    val data: String? = null
+    val message: String
 )
