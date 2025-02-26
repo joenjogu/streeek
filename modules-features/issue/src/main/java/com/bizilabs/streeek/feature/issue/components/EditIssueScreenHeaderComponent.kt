e44fbad4beaac2bcda77f6416fc1ef8c503a7692
bensalcie <bensalcie@gmail.com>
2025-02-26 23:31:25 +0300

    Fixes Edit Confirm button not visible on Light theme Mode

--- modules-features/issue/src/main/java/com/bizilabs/streeek/feature/issue/components/EditIssueScreenHeaderComponent.kt
+++ modules-features/issue/src/main/java/com/bizilabs/streeek/feature/issue/components/EditIssueScreenHeaderComponent.kt
@@ -5,19 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.rounded.DoneAll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -58,19 +56,9 @@ fun EditIssueScreenHeaderComponent(
                         IconButton(
                             onClick = onClickEditIssue,
                             enabled = true,
-                            colors =
-                                IconButtonDefaults.iconButtonColors(
-                                    contentColor =
-                                        if (state.isCreateActionEnabled) {
-                                            MaterialTheme.colorScheme.primary
-                                        } else {
-                                            MaterialTheme.colorScheme.onSurface.copy(0.25f)
-                                        },
-                                ),
                         ) {
                             Icon(
-                                tint = Color.White,
-                                imageVector = Icons.Default.Check,
+                                imageVector = Icons.Rounded.DoneAll,
                                 contentDescription = "Edit feedback",
                             )
                         }
