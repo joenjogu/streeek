4a4c4e0690304bd6ea333e69c9617967f1b72aff
theophiluskibet <kibettheophilus@gmail.com>
2025-03-04 07:48:32 +0300

    setup local module configuration

---
+++ shared-datasources/local/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.bizilabs.multliplatform)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.androidx.room)
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.androidx.room.runtime)
+            }
+
+            kotlin.srcDir("build/generated/ksp/metadata")
+        }
+    }
+}
+
+dependencies {
+    add("kspCommonMainMetadata", libs.androidx.room.compiler)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().configureEach {
+    if (name != "kspCommonMainKotlinMetadata") {
+        dependsOn("kspCommonMainKotlinMetadata")
+    }
+}
+
+android {
+    namespace = "com.bizilabs.streeek.lib.local"
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
